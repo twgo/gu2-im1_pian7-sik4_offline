1f569365af329515f7aefd65e9dba9919a450418
薛丞宏 <ihcaoe@gmail.com>
2017-02-17 11:04:49 +0800

    其他套件需要js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,12 +22,12 @@ module.exports = {
     }),
   ],
   resolve: {
-    extensions: ['.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
        {
-          test: /\.jsx$/,
+          test: /\.jsx?$/,
           use: ['babel-loader', 'strict-loader'],
           include: [path.join(__dirname, 'src')],
         },
