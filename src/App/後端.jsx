cafd0638f1041a761c7d008cf6d56e30abe6b3db
薛丞宏 <ihcaoe@gmail.com>
2017-02-17 15:05:35 +0800

    更新後端

--- src/App/後端.jsx
+++ src/App/後端.jsx
@@ -1,7 +1,7 @@
 export default class 後端  {
   static 網址() {
-    return 'http://localhost:8000/';
-    return 'https://db.itaigi.tw/';
+    //return 'http://localhost:8000/';
+    return 'http://順風耳.意傳.台灣/';
   }
 
   static 辦識音檔() {
@@ -13,7 +13,7 @@ export default class 後端  {
   }
 
   static 聽音檔(音檔) {
-    return encodeURI(this.網址()+'資料庫影音檔案/') + 音檔;
+    return encodeURI(this.網址() + '資料庫影音檔案/') + 音檔;
   }
 
 }
