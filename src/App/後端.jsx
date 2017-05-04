0648495873c3dfd00c055f1661cdd8f941efc67c
薛丞宏 <ihcaoe@gmail.com>
2017-05-04 17:28:28 +0800

    配合服務改api，改音檔的網址

--- src/App/後端.jsx
+++ src/App/後端.jsx
@@ -4,19 +4,19 @@ export default class 後端  {
     //return 'http://localhost:8000/';
 
     // https://服務.意傳.台灣/
-    return 'https://xn--lhrz38b.xn--v0qr21b.xn--kpry57d/';
+    return 'https://xn--lhrz38b.xn--v0qr21b.xn--kpry57d';
   }
 
   static 辦識音檔() {
-    return encodeURI(this.網址() + '辦識音檔');
+    return encodeURI(this.網址() + '/辦識音檔');
   }
 
   static 辦識結果() {
-    return encodeURI(this.網址() + '辦識結果');
+    return encodeURI(this.網址() + '/辦識結果');
   }
 
   static 聽音檔(音檔) {
-    return encodeURI(this.網址() + '資料庫影音檔案/') + 音檔;
+    return encodeURI(this.網址() + 音檔);
   }
 
 }
