4b8c7809223dcba368c8c13857a8518744c2e74d
a8568730 <a8568730@hotmail.com>
2017-11-14 17:31:16 +0800

    上傳程式移到錄好上傳.jsx

--- src/元件/錄好上傳.jsx
+++ src/元件/錄好上傳.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Debug from 'debug';
-import 音檔表 from './音檔表';
+import superagent from 'superagent-bluebird-promise';
+import 後端 from '../App/後端';
 
 var debug = Debug('itaigi:錄好上傳');
 
@@ -8,46 +9,57 @@ export default class 錄好上傳 extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      當佇送: false
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.state.當佇送 == false) {
+      this.fileInput.value = '';
+    }
+  }
+
+  handleClick(e) {
+    let fileInput = this.fileInput;
+    let 音檔 = fileInput.files[0];
+    this.setState({ 當佇送: true });
+    this.fileReader = new FileReader();
+    this.fileReader.onload = function () {
+        let encoded_blob = btoa(new Uint8Array(this.fileReader.result));
+        let { 全部確定的資料 } = this.state;
+        superagent.post(後端.辦識音檔())
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+          .send({
+            語言: '臺語',
+            blob: encoded_blob,
+          })
+          .then(({ body })=>(
+            this.setState({ 當佇送: false })
+          ))
+          .catch((err) => (
+            debug(err),
+            alert('上傳失敗，麻煩檢查網路或回報錯誤～'),
+            this.setState({ 當佇送: false })
+          ));
+      }.bind(this);
+
+    this.fileReader.readAsArrayBuffer(音檔);
   }
 
 
-
   render() {
-    let { 有確定的資料, 當佇送, 上傳好矣, 錄好的上傳 } = this.props;
-
-    if (!有確定的資料) {
-      return (
-        <div>
-        </div>
-        );
-    }
-
-    let { 確定的音檔, 漢字音標對齊 } = this.props.確定的資料;
-    if (上傳好矣) {
-      return (
-        <div className='ui segment'>
-          上傳好矣
-        </div>
-        );
-    }
-
-    if (當佇送) {
-      return (
-        <div className='ui segment'>
-          <div className="ui segment">
-            <div className="ui active inverted dimmer">
-              <div className="ui text loader">上傳中…</div>
-            </div>
-            <p></p>
-          </div>
-        </div>
-        );
-    }
-
     return (
-    <div className='ui segment'>
-      上傳失敗，請閣傳一擺<br/>
-    </div>
+      <section>
+        <input
+          type="file"
+          ref={(input) => { this.fileInput = input; }}/>
+        <button
+          className="ui compact blue labeled icon button"
+          onClick={this.handleClick.bind(this)}
+          disabled={this.state.當佇送}>
+            <i className="upload icon"/>上傳</button>
+      </section>
     );
   }
 }
--- src/頁/辨識.jsx
+++ src/頁/辨識.jsx
@@ -1,61 +1,19 @@
 import React from 'react';
 import Debug from 'debug';
-import superagent from 'superagent-bluebird-promise';
 import 辨識結果 from '../元件/辨識結果';
 import 錄好上傳 from '../元件/錄好上傳';
-import 後端 from '../App/後端';
 var debug = Debug('itaigi:辨識');
 
 class 辨識 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      當佇送: false,
-      上傳好矣: false
-    };
-  }
-
-  handleClick(e) {
-    let fileInput = this.fileInput;
-    let 音檔 = fileInput.files[0];
-    this.setState({ 當佇送: true, 上傳好矣: false });
-    this.fileReader = new FileReader();
-    this.fileReader.onload = function () {
-        let encoded_blob = btoa(new Uint8Array(this.fileReader.result));
-        let { 全部確定的資料 } = this.state;
-        superagent.post(後端.辦識音檔())
-          .set('Content-Type', 'application/x-www-form-urlencoded')
-          .send({
-            語言: '臺語',
-            blob: encoded_blob,
-          })
-          .then(({ body })=>(
-            this.setState({ 當佇送: false, 上傳好矣: true })
-          ))
-          .catch((err) => (
-            debug(err),
-            alert('上傳失敗，麻煩檢查網路或回報錯誤～'),
-            this.setState({ 當佇送: false })
-          ));
-      }.bind(this);
-
-    this.fileReader.readAsArrayBuffer(音檔);
   }
 
   render(){
     return (
     <div className='app container'>
       <h2>辨識</h2>
-      <section>
-        <input
-          type="file"
-          ref={(input) => { this.fileInput = input; }}/>
-        <button onClick={this.handleClick.bind(this)}>上傳</button>
-        {/*<錄好上傳
-            錄好的上傳={this.錄好的上傳.bind(this)}
-            確定的資料={全部確定的資料}
-            有確定的資料={有確定的資料} 當佇送={當佇送} 上傳好矣={上傳好矣}/>*/}
-      </section>
+      <錄好上傳/>
       <辨識結果/>
     </div>
     );
