25846bbbd20ec8b23adbd46938fd5a5188a2ff85
a8568730 <a8568730@hotmail.com>
2017-11-16 16:34:52 +0800

    依序送出request

--- src/元件/錄好上傳.jsx
+++ src/元件/錄好上傳.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Debug from 'debug';
-import superagent from 'superagent-bluebird-promise';
+import request from 'superagent-bluebird-promise';
 import 後端 from '../App/後端';
 
 var debug = Debug('itaigi:錄好上傳');
@@ -11,55 +11,55 @@ export default class 錄好上傳 extends React.Component {
     super(props);
     this.state = {
       當佇送: false,
-      請求數: 0,
-      上傳檔名: "選擇檔案..."
+      上傳檔名: "選擇檔案...",
+      encoded_blob: null
     };
   }
 
-  componentDidUpdate() {
-    if (this.state.請求數 == 3) {
-      this.fileInput.value = '';
-      this.setState({
-        當佇送: false,
-        請求數: 0,
-        上傳檔名: "選擇檔案..."
-      });
-    }
-  }
-
   handleClick(e) {
-    let fileInput = this.fileInput;
-    let 音檔 = fileInput.files[0];
+    let 音檔 = this.fileInput.files[0];
     this.fileReader = new FileReader();
     this.fileReader.onload = function () {
         let encoded_blob = btoa(new Uint8Array(this.fileReader.result));
-        this.setState({
-          當佇送: true,
-          請求數: 0
-        });
-        this.送出音檔('臺語', encoded_blob);
-        this.送出音檔('華語', encoded_blob);
-        this.送出音檔('臺華', encoded_blob);
+        this.送出所有語言的請求(encoded_blob);
       }.bind(this);
     this.fileReader.readAsArrayBuffer(音檔);
   }
 
-  送出音檔(語言, encoded_blob) {
-    superagent.post(後端.辦識音檔())
+  送出所有語言的請求(encoded_blob) {
+    this.setState({
+        encoded_blob,
+        當佇送: true
+    });
+    let promise臺語 = this.送出音檔('臺語');
+    let promise華語 = this.送出音檔('華語');
+    let promise臺華 = this.送出音檔('臺華');
+    Promise.all([promise臺華, promise華語, promise臺語])
+      .then(()=>{
+        console.log("all the files were created");
+          this.fileInput.value = '';
+          this.setState({
+            當佇送: false,
+            上傳檔名: "選擇檔案..."
+          });
+      }).catch((e, status) => {
+          console.log("error occurred", e, status);
+          this.fileInput.value = '';
+          this.setState({
+            當佇送: false,
+            上傳檔名: "選擇檔案..."
+          });
+      });
+  }
+  送出音檔(語言) {
+    let { encoded_blob } = this.state;
+    //return request promise
+    return request.post(後端.辦識音檔())
       .set('Content-Type', 'application/x-www-form-urlencoded')
       .send({
         語言,
         blob: encoded_blob,
-      })
-      .then((body)=>{
-        this.setState((prevState) => {
-          return {請求數: prevState.請求數 + 1};
-        })
-      })
-      .catch((err) => (
-        debug(err),
-        alert('上傳失敗，麻煩檢查網路或回報錯誤～')
-      ));
+      }).promise();
   }
 
   handleFileChange(e) {
