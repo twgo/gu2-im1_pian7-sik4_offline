3da12521f4841ea928a60a239cd47d436b42e36c
薛丞宏 <ihcaoe@gmail.com>
2018-05-24 15:44:31 +0800

    Update 後端.jsx

--- src/App/後端.jsx
+++ src/App/後端.jsx
@@ -8,11 +8,11 @@ export default class 後端  {
   }
 
   static 辦識音檔() {
-    return encodeURI(this.網址() + '/辦識音檔');
+    return encodeURI(this.網址() + '/辨識音檔');
   }
 
   static 辦識結果() {
-    return encodeURI(this.網址() + '/辦識結果');
+    return encodeURI(this.網址() + '/辨識結果');
   }
 
   static 聽音檔(音檔) {
